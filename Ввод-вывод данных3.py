3f99903a6529154a44ab702204a58cbc626074a3
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-09-19 01:14:15 +0400

    Add files via upload

---
+++ Ввод-вывод данных3.py
@@ -0,0 +1,6 @@
+a = int(input("a = ")) # вводим а
+b = int(input("b = ")) # вводим b
+l = int(input("l = ")) # вводим L
+N = int(input("N = ")) # вводим N
+L = 2 * l + 2 * (N - 1) * (b + a) + a # ищим искомую длину шнурка
+print(L) # вывод
