66647b4c3c8d38a6d28c2195af98db6fee7e236b
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-10-02 13:34:43 +0400

    02.10.21 циклы while

---
+++ 02.10.21.py
@@ -0,0 +1,25 @@
+
+#s = 120 * '1'
+
+#while True: 
+#	s0 = s
+#	while '111' in s0:
+#		s0 = s0.replace ('111', '2', 1)
+#		s0 = s0.replace ('222', '3', 1)
+#		s0 = s0.replace ('333', '1', 1)
+#	print(s0)
+#	break 
+
+#s = 17 * '1' + 30 * '2' + 30 * '3'
+
+#while '>1' in s or '>2' in s or '>3' in s:
+#	if '>1': 
+#		s = s.replace ('>1', '22>', 1)
+#	if '>1':
+#		s = s.replace ('>2', '2>1', 1)
+#	if '>1':
+#		s = s.replace ('>3', '1>', 1)
+#print(sum(int(x) for x in s if x!= '>'))
+
+
+
