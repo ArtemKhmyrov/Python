765f7d11b79451de4d5f346523c4fb74091d7f9a
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-10-09 00:25:50 +0400

    homework on 9.10.21

---
+++ 17_slide_while.py
@@ -0,0 +1,63 @@
+#n = int(input("Введите числа: "))
+#while n < 100 and n > 10:
+#    j= n % 10
+#    if ((j==3)) or (j==7):
+#        print (n)
+#    n = n + 1
+
+
+#a = int(input("Введита a: "))
+#b = int(input("Введита b: "))
+#c = int(input("Введита c: "))
+#while a <= b:
+#    if a % c == 0:
+#        print (a)
+#    a = a + 1
+
+
+#i = 10
+#n = 100
+#while i < n:
+#    a = i//10
+#    b = i%10
+#    if (a**2 + b**2) % 13 == 0:
+#        print(i)
+#    i = i + 1
+
+#i = 100 
+#sum = 0
+#while i < 1000:
+#    if i % 2 == 0:
+#        sum+=i
+#    i = i + 1
+#print(sum)
+ 
+
+#n = int(input("Введите некоторое значение: "))
+#count = 0
+#i = 100
+#while i < 1000:
+#    sum = 0
+#    for j in str(i):
+#        sum += int(j)
+#    i = i + 1
+#    if sum == n:
+#        count += 1
+#print(count)
+
+
+#sum = 0
+#i = 1
+#while i < 101:
+#    sum += i
+#    i = i + 1
+#print(sum)
+
+#n = int(input("Введите целое число: "))
+#m = int(input("Введите целое число: "))
+#i = 2
+#while i <= n:
+#    while i <= m:
+#        if (n % i == 0):
+#            print(i)
+#        i = i + 1
