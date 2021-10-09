cd736ed4d3a295dee974d38a61f6a168c4e03191
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-10-09 12:34:47 +0400

    lesson 2 on 9.10.21

---
+++ lesson 2 on 9.10.21 .py
@@ -0,0 +1,19 @@
+
+# задание 37 
+#def nProg(x, t):
+#    if x == t:
+#        return 1
+#    if x > t:
+#        return 0
+#    return (nProg(x + 1, t) + nProg(x + 3, t) + nProg(x + 6, t))
+#print(nProg(21, 30))
+
+
+#Задание 88
+#def nProg(x, t):
+#    if x == t:
+#        return 1
+#    if x > t:
+#        return 0
+#    return nProg(x + 1, t) + nProg(x + 3, t) + nProg(x * 2, t)
+#print(nProg(1, 4) * nProg(4,9) * nProg(9, 13))
